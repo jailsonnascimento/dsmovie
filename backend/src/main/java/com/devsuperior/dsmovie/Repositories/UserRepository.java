4cf149a0767bfb58c1f69384bc059d53f0519e7d
Jailson Santos do Nascimento <88466759+jailsonnascimento@users.noreply.github.com>
2022-03-13 21:33:54 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/Repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/Repositories/UserRepository.java
@@ -2,11 +2,9 @@ package com.devsuperior.dsmovie.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.dsmovie.entities.Movie;
-
 import com.devsuperior.dsmovie.entities.User;
 
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-	
+	User findByEmail(String email);
 }
